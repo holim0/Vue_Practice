c82929dcd48ee17e43548a8914e9140ab89c3641
holim0 <holim1226@gmail.com>
2021-07-19 16:55:02 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>This is an about page</h1>
+    {{ counter }}
+    {{ times2 }}
+    <button @click="inc">inc</button>
+  </div>
+</template>
+<script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const store = useStore();
+    const counter = computed(() => store.state.counter);
+    const test = computed(() => store.getters);
+    const inc = () => store.commit("setCounter", counter.value + 1);
+
+    return { counter, inc, test };
+  }
+};
+</script>
